33ef3828556067e7d915573afb175525e36669f0
gungdikaebs <gungdika88@gmail.com>
2023-07-10 17:34:06 +0800

    .

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,19 +4,26 @@ import { BiUser } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <section className="Navbar">
+    <section className="navbar">
       <div className="container-navbar">
-        <div className="left">
-          Twice <span>Dey</span>
-        </div>
-        <div className="right">
-          <div className="text">
-            <span>Home</span>
-            <span>Recommend</span>
-            <span>Stories</span>
+        <div className="content">
+          <div className="left">
+            Twice <span>Dey</span>
           </div>
-          <div className="profiles">
-            <BiUser />
+          <div className="right">
+            <div className="text">
+              <a href="">Home</a>
+              <a href="">Recommend</a>
+              <a href="">Stories</a>
+            </div>
+            <div className="profiles">
+              <BiUser
+                style={{
+                  fontSize: "1.5rem",
+                  color: "white",
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
